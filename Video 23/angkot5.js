16b31bd24e9b5b9004eb9462db817eadd89bdefe
Qu3s7 <wimardin.haris@gmail.com>
2020-05-22 16:53:51 +0700

    Create Video 23

---
+++ Video 23/angkot5.js
@@ -0,0 +1,13 @@
+var  jmlangkot = 10;
+var noangkot = 1;
+var angkotberoprasi = 6; 
+
+for(noangkot = 1; noangkot <= jmlangkot; noangkot++){
+	if (noangkot <= angkotberoprasi) {
+		console.log('Angkot No. '+ noangkot + ' Beroprasi')
+	} else if (noangkot === 8 || noangkot === 10 ) {
+		console.log('Angkot No. '+ noangkot + ' Sedang Lembur')
+	}else {
+		console.log('Angkot No. '+ noangkot + ' Tidak Beroprasi')
+	}
+}
