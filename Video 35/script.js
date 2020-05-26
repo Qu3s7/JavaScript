a2aa8e77d015dd8df89ba152c3f83dcf09720064
Qu3s7 <wimardin.haris@gmail.com>
2020-05-26 17:45:09 +0700

    Create Video 35

---
+++ Video 35/script.js
@@ -0,0 +1,38 @@
+// manipulasi array//
+
+// var arr = [];
+// arr[0]= "nama";
+// arr[1]= "2";
+// arr[2]= "3";
+
+// console.log(arr);
+
+//menghapus array
+
+// var arr= ["1","2","3"];
+// arr[1] = undefined;
+
+// console.log(arr);
+
+//menampilkan array
+
+// var arr= ["1","2","3"];
+
+// for( var i = 0; i<arr.length; i++){
+// 	console.log(arr[i]);
+// }
+
+//method
+//1.join
+var  arr = ['1','2','3'];
+// console.log(arr.join(' '));
+
+//2.push,pop
+// arr.push('4','5');
+// arr.pop();
+// console.log(arr.join(" "));
+
+//3.unshift,shift
+// arr.unshift('0');
+arr.shift();
+console.log(arr.join(" "));
